e75c1e67d52fecaeeef601f85a5e051f7b399fa3
Anasuya187 <anasuyadalabehera2003@gmail.com>
2026-01-08 22:42:03 +0530

    Add files via upload

---
+++ lib/main.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+import 'auth_service.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: LoginPage(),
+    );
+  }
+}
+
+class LoginPage extends StatelessWidget {
+  const LoginPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final AuthService authService = AuthService();
+
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Firebase Google Login'),
+      ),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () async {
+            final user = await authService.signInWithGoogle();
+
+            if (user != null) {
+              ScaffoldMessenger.of(context).showSnackBar(
+                SnackBar(
+                  content: Text(
+                    'Signed in as\n${user.displayName}\n${user.email}',
+                  ),
+                ),
+              );
+            }
+          },
+          child: const Text('Sign in with Google'),
+        ),
+      ),
+    );
+  }
+}
+
